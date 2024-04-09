ecd70cc413f27edc53ea2fd85d41571c4f75048e
Champoux2003 <82066604+Champoux2003@users.noreply.github.com>
2024-04-09 14:11:12 -0400

    Modified version

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia' 
 import router from '../router'
-import './style.css'
-import App from './App.vue'
+import '../style.css'
+import App from '../App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 //we initialize the pinia store in the main.ts file
 const app = createApp(App)
