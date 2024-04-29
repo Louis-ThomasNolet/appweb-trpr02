a809440add85910423e07c506644e66e7a605699
Louis-ThomasNolet <louis.thomas.nolet@icloud.com>
2024-04-28 21:47:33 -0400

    project finished

--- src/views/__tests__/AccueilViews.test.ts
+++ src/views/__tests__/AccueilViews.test.ts
@@ -5,13 +5,47 @@ import routes from '../../router/routes'
 import Accueil from '../Accueil.vue';
 //import ShipService from '../../scripts/shipService';
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes : routes
-})
+
 
 describe('Accueil.vue', () => {
+    it("Doit afficher un message d'erreur si le nom du joueur est vide", async () => {
+        const router = createRouter({
+            history: createWebHistory(),
+            routes : routes
+        })
+        const mockShipService = {
+            getShipList: Promise.resolve([{id: '3',name: 'Y-wing'}]),
+        };
+
+        const wrapper = mount(Accueil, {
+            global: {
+                plugins: [router],
+                mocks: {
+                    $router: router,
+                    $route: {name:'Bataille', query: {name: 'Player 1', selectedShip: '3'}},
+                    shipService: mockShipService
+                }
+            }
+        });
+    
+        await wrapper.find('#ship-select').setValue('3');
+        await wrapper.find('#name').setValue('');
+    
+        await wrapper.vm.$nextTick();
+        await wrapper.find('#start-button').trigger('click');
+        await router.isReady();
+    
+
+        // Check if the route has not changed
+        expect(router.currentRoute.value.name).toBe('Accueil');
+    });
+    
     it('Demarrer le jeu avec des donnees valides', async () => {
+        const router = createRouter({
+            history: createWebHistory(),
+            routes : routes
+        })
+
         const mockShipService = {
             getShipList: Promise.resolve([{id: '3',name: 'Y-wing'}]),
         };
@@ -39,32 +73,6 @@ describe('Accueil.vue', () => {
         expect(router.currentRoute.value.query.name).toBe('Player 1');
     });
 
-    it("Doit afficher un message d'erreur si le nom du joueur est vide", async () => {
-        const mockShipService = {
-            getShipList: Promise.resolve([{id: '3',name: 'Y-wing'}]),
-        };
-
-        const wrapper = mount(Accueil, {
-            global: {
-                plugins: [router],
-                mocks: {
-                    $router: router,
-                    $route: {name:'Bataille', query: {name: 'Player 1', selectedShip: '3'}},
-                    shipService: mockShipService
-                }
-            }
-        });
-
-        await wrapper.find('#name').setValue('');
-        await wrapper.find('#ship-select').setValue('3');
-
-
-        await wrapper.vm.$nextTick();
-        await wrapper.find('form').trigger('submit');
-        await router.isReady();
-
-        expect(router.currentRoute.value.name).toBe('Accueil');//Comme le nom est vide on doit rester sur la page d'accueil
-        
-    });
+    
 
 });
